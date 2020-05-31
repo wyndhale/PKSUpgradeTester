c7fabac421f5ad25e80e84989125ffe17ac5b7f5
wyndham-lewis <wyndhale@yahoo.co.uk>
2020-05-31 18:19:55 +0200

    Initial commit

---
+++ src/main/java/com/wyndhamlewis/utilities/pks/PksUpgradeTesterApplication.java
@@ -0,0 +1,13 @@
+package com.wyndhamlewis.utilities.pks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PksUpgradeTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PksUpgradeTesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wyndhamlewis/utilities/pks/PksUpgradeTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyndhamlewis.utilities.pks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PksUpgradeTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
